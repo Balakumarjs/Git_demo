0832a2a3ec5a61013a1843dc9c1187aba0da8bb3
Balakumarjs <balakumarvcet@gmail.com>
2024-12-13 13:00:31 +0530

    initial commit

---
+++ src/demo/Example.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Example 
+{
+    public void m1()
+    {
+    	System.out.println("java");
+    }
+	public static void main(String[] args)
+	{
+		Example a = new Example();
+		a.m1();
+	}
+
+}
