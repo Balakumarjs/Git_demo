7e43117b2d02f85f64706d83faa8d519973527a6
Balakumarjs <balakumarvcet@gmail.com>
2024-12-13 13:09:41 +0530

    Added new program
    Second commit

---
+++ src/demo/Example2.java
@@ -0,0 +1,12 @@
+public class Sample
+  {
+    pulic void main a1()
+    {
+      system.out.println("selenium");
+    }
+    public static void main(String args[])
+    {
+      Sample s = new Sample();
+      s.a1();
+    }
+  }
